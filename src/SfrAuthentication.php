9a180593b55b0f10d1952109762a4b6686ddb9f5
jonathanoff2022 <jonathanoff2022@gmail.com>
2022-07-16 20:36:30 +0200

    Generate random fingerprint for anonymity

--- src/SfrAuthentication.php
+++ src/SfrAuthentication.php
@@ -8,12 +8,37 @@ use GuzzleHttp\Exception\GuzzleException;
 
 class SfrAuthentication
 {
+    private const HEXADECIMAL_ALPHABET = "0123456789abcdef";
+
     private const CREATE_TOKEN_URL = "https://www.sfr.fr/cas/services/rest/3.2/createToken.json";
     private const TOKEN_DURATION = 86400; // Same as the application
 
     private const USER_AGENT = 'SFRMonCompte/10.3.0 (com.sfr.android.moncompte; build:10303000; Android OS 12) okhttp/4.9.3';
 
     private Client $client;
+
+    private static function generateFingerprint(): string
+    {
+        // Format : $brand | $modelNumber | $id
+
+        $brand = "BRAND";
+        $modelNumber = "MODEL-NUMBER";
+
+        try {
+            $id = bin2hex(random_bytes(8));
+        } catch (Exception $exception) {
+            // "random_bytes" is not able to find a good source to generate random bytes.
+            // But this generation does not need to be very secure.
+            // Therefore, we will directly generate a hex string using "rand"
+            $hexAlphabetLen = strlen(self::HEXADECIMAL_ALPHABET);
+            $id = '';
+            for ($index = 0; $index < 8; $index++) {
+                $id .= self::HEXADECIMAL_ALPHABET[rand(0, $hexAlphabetLen)];
+            }
+        }
+
+        return $brand . ' | ' . $modelNumber . ' | ' . $id;
+    }
 
     /**
      * @param string|null $proxy The proxy to use or null
@@ -40,7 +65,7 @@ class SfrAuthentication
                 'auth' => [$username, $password],
                 'headers' => [
                     'Secret' => base64_encode('SFRETMoiAndroidV1:windows1980'),
-                    'Fingerprint' => base64_encode('BRAND | MODEL_NUMBER | 8_BYTE_HEX'),
+                    'Fingerprint' => base64_encode(self::generateFingerprint()),
                     'User-Agent' => self::USER_AGENT
                 ]
             ]);
