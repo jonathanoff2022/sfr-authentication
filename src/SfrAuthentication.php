cf83ae938f8ef95661fa6090140bd0b779bf3ca8
jonathanoff2022 <jonathanoff2022@gmail.com>
2022-07-16 20:23:33 +0200

    Send create token request

--- src/SfrAuthentication.php
+++ src/SfrAuthentication.php
@@ -4,9 +4,15 @@ namespace Sfr;
 
 use Exception;
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
 
 class SfrAuthentication
 {
+    private const CREATE_TOKEN_URL = "https://www.sfr.fr/cas/services/rest/3.2/createToken.json";
+    private const TOKEN_DURATION = 86400; // Same as the application
+
+    private const USER_AGENT = 'SFRMonCompte/10.3.0 (com.sfr.android.moncompte; build:10303000; Android OS 12) okhttp/4.9.3';
+
     private Client $client;
 
     /**
@@ -29,6 +35,20 @@ class SfrAuthentication
      */
     public function authenticate(string $username, string $password): string
     {
+        try {
+            $response = $this->client->get(self::CREATE_TOKEN_URL, [
+                'auth' => [$username, $password],
+                'headers' => [
+                    'Secret' => base64_encode('SFRETMoiAndroidV1:windows1980'),
+                    'Fingerprint' => base64_encode('BRAND | MODEL_NUMBER | 8_BYTE_HEX'),
+                    'User-Agent' => self::USER_AGENT
+                ]
+            ]);
+        } catch (GuzzleException $exception) {
+            // TODO : Handle GuzzleException
+            throw new Exception("Handling GuzzleException is not yet implemented", $previous = $exception);
+        }
+
         // TODO : Implement authenticate function
         throw new Exception("Function authenticate is not yet implemented");
     }
