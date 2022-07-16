1aec638932eb4a6b1298fac4aeecdf7354966fa6
jonathanoff2022 <jonathanoff2022@gmail.com>
2022-07-16 20:39:54 +0200

    Move id generation to a function

--- src/SfrFingerprint.php
+++ src/SfrFingerprint.php
@@ -13,25 +13,33 @@ class SfrFingerprint
         return "MODEL-NUMBER";
     }
 
+    private static function generateId(): string {
+
+        try {
+            return bin2hex(random_bytes(8));
+        } catch (Exception $exception) {
+            // "random_bytes" is not able to find a good source to generate random bytes.
+            // But this generation does not need to be very secure.
+            // Therefore, we will directly generate a hex string using "rand"
+
+            $id = '';
+
+            $hexAlphabetLen = strlen(self::HEXADECIMAL_ALPHABET);
+            for ($index = 0; $index < 8; $index++) {
+                $id .= self::HEXADECIMAL_ALPHABET[rand(0, $hexAlphabetLen)];
+            }
+
+            return $id;
+        }
+    }
+
     public static function generateFingerprint(): string
     {
         // Format : $brand | $modelNumber | $id
 
         $brand = self::generateBrand();
         $modelNumber = self::generateModelNumber();
-
-        try {
-            $id = bin2hex(random_bytes(8));
-        } catch (Exception $exception) {
-            // "random_bytes" is not able to find a good source to generate random bytes.
-            // But this generation does not need to be very secure.
-            // Therefore, we will directly generate a hex string using "rand"
-            $hexAlphabetLen = strlen(self::HEXADECIMAL_ALPHABET);
-            $id = '';
-            for ($index = 0; $index < 8; $index++) {
-                $id .= self::HEXADECIMAL_ALPHABET[rand(0, $hexAlphabetLen)];
-            }
-        }
+        $id = self::generateId();
 
         return $brand . ' | ' . $modelNumber . ' | ' . $id;
     }
