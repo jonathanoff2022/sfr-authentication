984ab10b6998c3beb46b916e6851ef5d76b7453a
jonathanoff2022 <jonathanoff2022@gmail.com>
2022-07-16 22:29:35 +0200

    Create class SfrException

---
+++ src/SfrException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace SfrAuthentication;
+
+use Exception;
+use Throwable;
+
+class SfrException extends Exception
+{
+    public function __construct(string $message = "", int $code = 0, ?Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+}
