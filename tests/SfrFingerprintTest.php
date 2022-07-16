ec209ee722ebee1f4a188e2b3f98367985a30741
jonathanoff2022 <jonathanoff2022@gmail.com>
2022-07-16 22:18:08 +0200

    Update doc of function checkFormat

--- tests/SfrFingerprintTest.php
+++ tests/SfrFingerprintTest.php
@@ -9,6 +9,7 @@ final class SfrFingerprintTest extends TestCase
     private const FINGERPRINT_FORMAT_REGEX = "/^([A-Za-z0-9 ]+) \| ([A-Za-z0-9- ]+) \| ([0-9a-f]{16})$/";
 
     /**
+     * @param string $fingerprint The fingerprint to check
      * @return void
      * @test
      * @dataProvider fingerprintProvider
