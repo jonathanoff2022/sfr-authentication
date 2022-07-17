6967c67bdbbdeceb42f62c5fe0987693b9251e24
jonathanoff2022 <jonathanoff2022@gmail.com>
2022-07-17 14:42:08 +0200

    Add test for account locked exception

--- tests/SfrAuthenticationTest.php
+++ tests/SfrAuthenticationTest.php
@@ -35,6 +35,33 @@ final class SfrAuthenticationTest extends TestCase
         }
     }
 
+    /**
+     * @param string $username The username of the account
+     * @param string $password The password of the account
+     * @return void
+     * @test
+     * @dataProvider credentialsProvider
+     */
+    public function authenticationLockedTest(string $username, string $password): void
+    {
+        // TODO : Update the account locked response with a real response
+
+        $mockHandler = new MockHandler([
+            new Response(403, [], '{"createToken":{"code":"ACCOUNT_LOCKED_EXCEPTION","message":"*TODO*"}}'),
+        ]);
+
+        $service = new SfrAuthentication(null, HandlerStack::create($mockHandler));
+
+        try {
+            $token = $service->authenticate($username, $password);
+            $this->fail("Authenticated with token : " . $token);
+        } catch (SfrException $exception) {
+            $this->assertInstanceOf(SfrAuthenticationException::class, $exception);
+            $this->assertEquals(SfrAuthenticationException::ACCOUNT_LOCKED, $exception->authenticationResult);
+            // TODO : Compare exception message
+        }
+    }
+
     public function credentialsProvider(): array
     {
         return [
